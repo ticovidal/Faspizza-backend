ed4b1334ae18530ba9f3738a590064f0eb5fa1ca
Thiago Vidal <tvidal82@gmail.com>
2026-03-30 05:11:51 -0300

    fix: add cors to backend

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,26 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',
+      'https://fast-pizza-frontend.vercel.app',
+    ],
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('FastPizza API')
+    .setDescription('API do sistema de pedidos FastPizza')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
